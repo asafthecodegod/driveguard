6d505aade53358b550ecf9cb6afb47ae8f0fb190
asafthecodegod <asafavisar5@gmail.com>
2024-12-12 12:17:55 +0200

    a

--- app/src/main/java/com/example/asaf_avisar/activitys/test.java
+++ app/src/main/java/com/example/asaf_avisar/activitys/test.java
@@ -17,6 +17,6 @@ public class test extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_test);
-
+        System.out.println("Hello");
     }
 }
